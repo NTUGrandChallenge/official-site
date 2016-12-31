95a77b3d09a4b1cb1489cd8c55502585d1a71c37
XXLee <leechinghon@gmail.com.tw>
2016-12-31 18:10:06 +0800

    update json format

--- assets/js/judge.js
+++ assets/js/judge.js
@@ -4,22 +4,16 @@ var judges = new Vue({
     data: {
         judges: []
     },
-    computed: {
-        smartLife: function(){
-            console.log(this.judges.filter(function(e){ return e.field == 'sl'}))
-            return this.judges.filter(function(e){ return e.field == 'sl'})
-        }
-    },
     created: function(){
-        console.log("create")
         this.fetchData();
     },
     methods: {
         fetchData: function(){
-        console.log("fetch")
             var self = this;
-            $.get('assets/js/judges.json', function(data){
+            $.getJSON('assets/js/judges.json', function(data){
                 self.judges = data;
+                console.log(data)
+                console.log("hello")
             })
         }
     }
