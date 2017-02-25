43ca3851f5844182804a577c095b8d6b4aa7a376
XXLeeS <leechinghon@gmail.com>
2017-02-26 04:28:41 +0800

    add shuffle to teams index data

--- assets/js/20teams-vue.js
+++ assets/js/20teams-vue.js
@@ -68,6 +68,23 @@ var teams = new Vue({
                 self.$set(v, 'coverImgSrc', coverImgSrc);
                 self.$set(v, 'link', link);
             });
+        },
+        shuffle: function(array){
+            // Fisher-Yates (aka Knuth) Shuffle
+            var currentIndex = array.length;
+            var tmpValue, randomIndex;
+
+            while(currentIndex !== 0){
+                // Pick a remaining element...
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex -= 1;
+
+                // And swap it with the current element.
+                tmpValue = array[currentIndex];
+                array[currentIndex] = array[randomIndex];
+                array[randomIndex] = tmpValue;
+            }
+            return array;
         }
     }
 })
