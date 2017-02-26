785f35e051d134508c294e9b093fda98266b0c4b
XXLeeS <leechinghon@gmail.com>
2017-02-26 23:48:09 +0800

    update data

--- lib/20teams.r
+++ lib/20teams.r
@@ -31,38 +31,38 @@ getMembers <- function(i){
   members = list()
   if(data[i,]$name1 != ''){
     tmp = list()
-    tmp[["name"]] = data[i,]$name1
-    tmp[["role"]] = data[i,]$role1
+    tmp[["name"]] = slashNToBr(data[i,]$name1)
+    tmp[["role"]] = slashNToBr(data[i,]$role1)
     members[[1]] = tmp
   }
   if(data[i,]$name2 != ''){
     tmp = list()
-    tmp[["name"]] = data[i,]$name2
-    tmp[["role"]] = data[i,]$role2
+    tmp[["name"]] = slashNToBr(data[i,]$name2)
+    tmp[["role"]] = slashNToBr(data[i,]$role2)
     members[[2]] = tmp
   }
   if(data[i,]$name3 != ''){
     tmp = list()
-    tmp[["name"]] = data[i,]$name3
-    tmp[["role"]] = data[i,]$role3
+    tmp[["name"]] = slashNToBr(data[i,]$name3)
+    tmp[["role"]] = slashNToBr(data[i,]$role3)
     members[[3]] = tmp
   }
   if(data[i,]$name4 != ''){
     tmp = list()
-    tmp[["name"]] = data[i,]$name4
-    tmp[["role"]] = data[i,]$role4
+    tmp[["name"]] = slashNToBr(data[i,]$name4)
+    tmp[["role"]] = slashNToBr(data[i,]$role4)
     members[[4]] = tmp
   }
   if(data[i,]$name5 != ''){
     tmp = list()
-    tmp[["name"]] = data[i,]$name5
-    tmp[["role"]] = data[i,]$role5
+    tmp[["name"]] = slashNToBr(data[i,]$name5)
+    tmp[["role"]] = slashNToBr(data[i,]$role5)
     members[[5]] = tmp
   }
   if(data[i,]$name6 != ''){
     tmp = list()
-    tmp[["name"]] = data[i,]$name6
-    tmp[["role"]] = data[i,]$role6
+    tmp[["name"]] = slashNToBr(data[i,]$name6)
+    tmp[["role"]] = slashNToBr(data[i,]$role6)
     members[[6]] = tmp
   }
   return(members)
@@ -71,12 +71,12 @@ getMembers <- function(i){
 data = read.table("data.csv", header=TRUE, sep=",")
 
 l = lapply(1:nrow(data), FUN=function(i){
-  list(team_name = data[i,]$team_name,
+  list(team_name = slashNToBr(data[i,]$team_name),
        team_id = data[i,]$team_id,
        field = data[i,]$field,
-       slogan = data[i,]$slogan,
+       slogan = slashNToBr(data[i,]$slogan),
        observation = slashNToBr(data[i,]$observation),
-       challenge_definition = data[i,]$challenge_definition,
+       challenge_definition = slashNToBr(data[i,]$challenge_definition),
        product_introduction = slashNToBr(data[i,]$product_introduction),
        team_introduction = slashNToBr(data[i,]$team_introduction),
        self_question = data[i,]$self_question,
