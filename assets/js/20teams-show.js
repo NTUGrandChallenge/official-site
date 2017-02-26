aff12e8da2f98c485e3156e554ab8aa4eac19c5c
XXLeeS <leechinghon@gmail.com>
2017-02-27 02:17:24 +0800

    show20 mobile ver

--- assets/js/20teams-show.js
+++ assets/js/20teams-show.js
@@ -1,14 +1,16 @@
-$('#single_team').fullpage({
-    scrollOverflow: true,
-    verticalCentered: false,
-    onLeave: function(index, nextIndex, direction){
-        // rebuid for long content
-        if(index == 3 && direction == 'down'){
+if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    // fullpage only active on desktop
+    $('#single_team').fullpage({
+        scrollOverflow: true,
+        verticalCentered: false,
+        onLeave: function(index, nextIndex, direction){
+            // rebuid for long content
+            if(index == 3 && direction == 'down'){
+                $.fn.fullpage.reBuild();
+            }
+        },
+        afterResize: function(){
             $.fn.fullpage.reBuild();
-        }
-    },
-    afterResize: function(){
-        $.fn.fullpage.reBuild();
-    },
-
-});
+        },
+    });
+}
