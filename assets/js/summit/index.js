2eabcad42d12ee353eef873f4fd2e3675a4b0934
XXLeeS <leechinghon@gmail.com>
2017-03-25 17:00:11 +0800

    mobile sidenav

--- assets/js/summit/index.js
+++ assets/js/summit/index.js
@@ -1,6 +1,7 @@
 var nav_width = 300;
 var nav_transition_time = 400;
 
+// sidenav expand control
 $('#sidenav').hover(function(){
 	$(this).find('.list.main').addClass('expanded');
 }, function(){
@@ -9,7 +10,6 @@ $('#sidenav').hover(function(){
 		next();
 	});
 });
-
 $('#sidenav .list.main li.hasSub').hover(function(){
 	$(this).find('.list.sub').delay(nav_transition_time).queue(function(next){
 		$(this).addClass('expanded');
@@ -19,3 +19,12 @@ $('#sidenav .list.main li.hasSub').hover(function(){
 	$(this).find('.list.sub').clearQueue();
 	$(this).find('.list.sub').removeClass('expanded');
 });
+
+// sidnav exit and back control on mobile
+$('#sidenav .close_button').click(function(){
+	$('#sidenav .list').removeClass('expanded');
+	$('#sidenav').trigger('mouseleave');	//not work QQ
+})
+$('#sidenav .list.sub .back_button').click(function(){
+	$('#sidenav .list.sub').removeClass('expanded');
+})
