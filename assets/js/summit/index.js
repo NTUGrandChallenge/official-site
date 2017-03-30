4f37eb72c4a27fcf38447bb20bb19ef37d4fabd3
XXLeeS <leechinghon@gmail.com>
2017-03-30 22:42:48 +0800

    update some rwd fuck you

--- assets/js/summit/index.js
+++ assets/js/summit/index.js
@@ -1,3 +1,8 @@
+// top animation
+$(window).on("load", function(){
+	$('#top').addClass('animation-start');
+})
+
 //five activity
 for(var i = 1; i <= $('#five .activity').length; i++){
     $('#five .activity:nth-child('+ i +')').hover(function(){
