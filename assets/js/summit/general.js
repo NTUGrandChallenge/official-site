70abd49f1a80f2e2f3190db09eea25298432b9a4
XXLeeS <leechinghon@gmail.com>
2017-04-04 00:28:26 +0800

    fix some bug about screen size

--- assets/js/summit/general.js
+++ assets/js/summit/general.js
@@ -12,16 +12,16 @@ var startSidenav = function(){
 		});
 	});
 	$('#sidenav .list.main li.hasSub').hover(function(){
-		var self = $(this);
-		setTimeout(function(){
-			self.find('.list.sub').addClass('expanded');
-		}, 400)
-		// $(this).find('.list.sub').delay(nav_transition_time).queue(function(next){
-		// 	$(this).addClass('expanded');
-		// 	next();
-		// });
+		// var self = $(this);
+		// setTimeout(function(){
+		// 	self.find('.list.sub').addClass('expanded');
+		// }, 400)
+		$(this).find('.list.sub').delay(nav_transition_time).queue(function(next){
+			$(this).addClass('expanded');
+			next();
+		});
 	}, function(){
-		// $(this).find('.list.sub').clearQueue();
+		$(this).find('.list.sub').clearQueue();
 		$(this).find('.list.sub').removeClass('expanded');
 	});
 
@@ -34,8 +34,6 @@ var startSidenav = function(){
 		$('#sidenav .list.sub').removeClass('expanded');
 	});
 };
-
-console.log("FUCK")
 
 $('#sidenav').load('_sidenav.html', startSidenav);
 $('footer').load('_footer.html');
