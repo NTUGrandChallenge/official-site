ce9e813ea371bd7ac46aa5bb06223ed6b6b0368a
XXLeeS <leechinghon@gmail.com>
2017-04-04 21:58:44 +0800

    fix sidenav bug

--- assets/js/summit/general.js
+++ assets/js/summit/general.js
@@ -2,20 +2,21 @@ var nav_width = 300;
 var nav_transition_time = 400;
 
 var startSidenav = function(){
-// sidenav expand control
+	// sidenav expand control
 	$('#sidenav').hover(function(){
 		$(this).find('.list.main').addClass('expanded');
+		$(this).find('.list.main').delay(nav_transition_time).queue(function(next){
+			$(this).css('overflow', 'visible');
+			next();
+		});
 	}, function(){
 		$(this).find('.list.main').delay(nav_transition_time).queue(function(next){
 			$(this).removeClass('expanded');
+			$(this).css('overflow', 'hidden');
 			next();
 		});
 	});
 	$('#sidenav .list.main li.hasSub').hover(function(){
-		// var self = $(this);
-		// setTimeout(function(){
-		// 	self.find('.list.sub').addClass('expanded');
-		// }, 400)
 		$(this).find('.list.sub').delay(nav_transition_time).queue(function(next){
 			$(this).addClass('expanded');
 			next();
@@ -28,6 +29,7 @@ var startSidenav = function(){
 	// sidnav exit and back control on mobile
 	$('#sidenav .close_button').click(function(){
 		$('#sidenav .list').removeClass('expanded');
+		$('#sidenav .list').css('overflow', 'hidden');
 		$('#sidenav').trigger('mouseleave');	//not work QQ
 	});
 	$('#sidenav .list.sub .back_button').click(function(){
