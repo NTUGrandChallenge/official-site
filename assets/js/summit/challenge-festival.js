281a824e257ba11efc1689722ae11e526ce7ac7d
ShaoEnChen <seanchen47@gmail.com>
2017-04-25 20:00:38 +0800

    refine challenge festival - challenge festival

---
+++ assets/js/summit/challenge-festival.js
@@ -0,0 +1,41 @@
+/*
+ * 切換挑戰賽嘉年華版面
+ */
+
+var act1 = $('#act1');
+var act2 = $('#act2');
+var act3 = $('#act3');
+var act4 = $('#act4');
+var acts = [act1, act2, act3, act4];
+
+// Initialize view when enter the page
+act2.hide();
+act3.hide();
+act4.hide();
+
+// Apply function to show only the act clicked
+function toggleAct( index ) {
+	var acts_to_hide = acts.slice();
+	acts_to_hide[index - 1].show();
+	// Pop the act to toggle, the remaining should be hidden
+	acts_to_hide.splice( index - 1, 1 );
+	$.each(acts_to_hide, function(key, val) {
+		val.hide();
+	});
+}
+
+$('#btn-act1').click(function() {
+	toggleAct(1);
+});
+
+$('#btn-act2').click(function() {
+	toggleAct(2);
+});
+
+$('#btn-act3').click(function() {
+	toggleAct(3);
+});
+
+$('#btn-act4').click(function() {
+	toggleAct(4);
+});
